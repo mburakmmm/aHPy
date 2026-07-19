b8dc12f674c30f60588f5231fb8217c315cd0215
Melih Burak Memiş <mburakmemiscy@gmail.com>
2026-07-19 16:37:31 +0300

    Preserve C linkage for benchmark fixture

--- tests/ahpy/benchmark_external.h
+++ tests/ahpy/benchmark_external.h
@@ -1,6 +1,14 @@
 #ifndef AHPY_BENCHMARK_EXTERNAL_H
 #define AHPY_BENCHMARK_EXTERNAL_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 long long ahpy_benchmark_external_add(long long left, long long right);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
